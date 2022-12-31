43a5f3cf3936656eb04ce57879f0a61c88131701
wylderM <wyldermenezes@gmail.com>
2022-12-31 15:00:29 -0300

    projeto configurado

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
+require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const router = require('./routes');
 const app = express();
+const PORT = process.env.SERVER_PORT;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use(router);
+
+app.listen(PORT, () => {
+   console.log('Api rodando na porta: ', PORT);
+});
